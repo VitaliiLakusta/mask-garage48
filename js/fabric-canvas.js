f22f5b24ead7a5db0797e906eb88b4717f4c240e
Vitalii Lakusta <lakusta96@gmail.com>
2015-05-09 18:08:09 +0300

    factored out js code for fabric canvas

---
+++ js/fabric-canvas.js
@@ -0,0 +1,75 @@
+
+var gMaskImg = null;
+var gCanvas = null;
+var gUserImgUrl = './img/pug.jpg';
+
+(function() {
+  var canvas = this.__canvas = new fabric.Canvas('c');
+  // canvas.calcOffset();
+  fabric.Object.prototype.transparentCorners = false;
+  
+  canvas.setHeight(1024);
+  canvas.setWidth(768);  
+  canvas.setBackgroundImage(gUserImgUrl, canvas.renderAll.bind(canvas));
+  var padding = 0;
+  
+  fabric.Image.fromURL('./img/transparent_mask_optimized.png', function(img) {
+  //fabric.Image.fromURL('./img/pug.jpg', function(img) {
+
+  gMaskImg=img;
+  gCanvas=canvas;
+  
+    img.lockRotation = true;
+    img.scaleToWidth(800);  
+  canvas.add(img);
+
+
+
+    document.getElementById('img-width').onchange = function() {
+      img.scaleToWidth(parseInt(this.value, 10));
+      canvas.renderAll();
+    };
+    document.getElementById('img-angle').onchange = function() {
+      img.setAngle(this.value);
+      canvas.renderAll();
+    };
+    document.getElementById('img-padding').onchange = function() {
+      padding = parseInt(this.value, 10);
+      canvas.renderAll();
+    };
+    document.getElementById('img-offset-x').onchange = function() {
+      pattern.offsetX = parseInt(this.value, 10);
+      canvas.renderAll();
+    };
+    document.getElementById('img-offset-y').onchange = function() {
+      pattern.offsetY = parseInt(this.value, 10);
+      canvas.renderAll();
+    };
+    document.getElementById('img-repeat').onclick = function() {
+      pattern.repeat = this.checked ? 'repeat' : 'no-repeat';
+      canvas.renderAll();
+    };
+  });
+  
+})();
+
+function saveModifications() {
+  console.log(gMaskImg.getLeft());
+  console.log(gMaskImg.getTop());
+  console.log(gMaskImg.getHeight());
+  console.log(gMaskImg.getWidth());  
+  console.log(gCanvas.getBackgroundImage());
+  //console.log(maskImg.getAngleInRadians());
+}  
+
+function changeBackgroundImage() {
+  //var canvas = this.__canvas = new fabric.Canvas('c');
+  //fabric.Object.prototype.transparentCorners = false;  
+  //gCanvas=canvas;  
+  
+  var url=document.getElementById('imgUrl').value;
+  gUserImgUrl=url;
+  gCanvas.setHeight(1024);
+  gCanvas.setWidth(768);
+  gCanvas.setBackgroundImage(gUserImgUrl, gCanvas.renderAll.bind(gCanvas));
+}
